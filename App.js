7b2f9d11a4cc9086451c458017828824dabad2dc
Che To <chekinto2@hotmail.com>
2020-12-17 12:50:07 +0000

    test

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Gift of Death</Text>
       <StatusBar style="auto" />
+      <FontAwesomeIcon icon="coffee" />
     </View>
   );
 }
